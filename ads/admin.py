7edebedfa9f77ca34f59673fc298630747f3bc29
maryamaghaei <maryamaghaei74@gmali.com>
2025-12-13 20:38:52 +0330

    Initial setup: core project, ads app, allauth, media config

--- ads/admin.py
+++ ads/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Category, Ad, AdImage
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(Ad)
+class AdAdmin(admin.ModelAdmin):
+    list_display = ['title', 'user', 'category', 'price', 'created_at']
+    list_filter = ['category', 'created_at']
+    search_fields = ['title', 'description']
+
+    admin.site.register(AdImage)
--- ads/models.py
+++ ads/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=100, unique=True)
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+    def __str__(self):
+        return self.name
+
+
+class Ad(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.IntegerField()
+    phone = models.CharField(max_length=15)
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.title
+
+
+class AdImage(models.Model):
+    ad = models.ForeignKey(Ad, on_delete=models.CASCADE, related_name='images')
+    image = models.ImageField(upload_to='ads/')
