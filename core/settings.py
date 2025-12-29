b9ded75aece54367195b619a332508b65b9fdac9
maryamaghaei <maryamaghaei74@gmali.com>
2025-12-29 21:51:32 +0330

    Secure SECRET_KEY using environment variable

--- core/settings.py
+++ core/settings.py
@@ -20,7 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6y0wv4-y)w^+%#-)49f=x!#6npk608^38350&8vm)2cz0j3z7='
+#SECRET_KEY = 'django-insecure-6y0wv4-y)w^+%#-)49f=x!#6npk608^38350&8vm)2cz0j3z7='
+
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-default-key-for-local-only')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
